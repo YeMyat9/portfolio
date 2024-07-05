85ccb23ba8d94ae0b5d57f55cab38968c6bab58a
Ye Myat Moe <160022780+YeMyat9@users.noreply.github.com>
2024-07-05 22:30:46 +0700

    Add files via upload

---
+++ me.js
@@ -0,0 +1,45 @@
+const socialButton1 = document.getElementById('social-button1');
+socialButton1.addEventListener('click', function() {
+    window.location.href = 'https://www.instagram.com/dest_voyage_/';
+});
+
+const socialButton2 = document.getElementById('social-button2');
+socialButton2.addEventListener('click', function() {
+    window.location.href = 'https://www.linkedin.com/in/ye-myat-moe-2a6105230/';
+});
+
+const socialButton3 = document.getElementById('social-button3');
+socialButton3.addEventListener('click', function() {
+    window.location.href = 'https://github.com/YeMyat144';
+});
+
+const pro1 = document.getElementById('p1');
+pro1.addEventListener('click', function() {
+    window.location.href = 'https://yemyat144.github.io/YOLO/';
+});
+
+const pro2 = document.getElementById('p2');
+pro2.addEventListener('click', function() {
+<<<<<<< HEAD
+    window.location.href = 'https://yemyat144.github.io/Menu_Page_javascript/';
+=======
+    window.location.href = 'https://yemyat144.github.io/Menu_Page_javascript/maryjane/';
+>>>>>>> f83c8b9f30635de8735051f8fc5b04647b63d0b1
+});
+const pro3 = document.getElementById('p3');
+pro3.addEventListener('click', function() {
+    window.location.href = 'https://yemyat144.github.io/Etch-a-Sketch/';
+});
+// const pro4 = document.getElementById('p4');
+// pro4.addEventListener('click', function() {
+//     window.location.href = 'https://yemyat144.github.io/Etch-a-Sketch/';
+// });
+<<<<<<< HEAD
+=======
+const pro4 = document.getElementById('p4');
+pro4.addEventListener('click', function() {
+    window.location.href = 'https://yemyat144.github.io/YOLO/';
+});
+>>>>>>> f83c8b9f30635de8735051f8fc5b04647b63d0b1
+
+
